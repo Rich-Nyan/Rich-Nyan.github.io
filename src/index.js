b94425390ad6dcb8d133867125598e36e2b17cba
Rich-Nyan <ryan.chin.012@gmail.com>
2025-03-22 18:41:47 -0400

    analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, useLocation} from "react-router-dom";
 
 import App from "./App";
 import About from './components/About';
@@ -10,14 +10,32 @@ import Projects from './components/Projects';
 import NotFound from './components/NotFound'; 
 import Resume from './components/Resume';
 
+// Google Analytics setup
+const trackPageView = (url) => {
+  if (window.gtag) {
+    window.gtag("config", "G-H9EY6S7SQR", { page_path: url });
+  }
+};
+
+// Component to track route changes
+const Analytics = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    trackPageView(location.pathname);
+  }, [location]);
+
+  return null;
+};
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<App />}>
-      <Route path="/" element={<About />} />
-      <Route path="/coursework" element={<Coursework />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/resume" element={<Resume />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<><Analytics /><About /></>} />
+      <Route path="/coursework" element={<><Analytics /><Coursework /></>} />
+      <Route path="/projects" element={<><Analytics /><Projects /></>} />
+      <Route path="/resume" element={<><Analytics /><Resume /></>} />
+      <Route path="*" element={<><Analytics /><NotFound /></>} />
     </Route>
   )
 );
